f94d8cb565f3c8216e7f32991a09d7ab0fae2989
karthikeyan-dev3 <karthi.online03@gmail.com>
2026-04-24 14:08:59 +0530

    str

--- bronze/bronze_script.py
+++ bronze/bronze_script.py
@@ -1,14 +1,14 @@
 from awsglue.context import GlueContext
 from pyspark.context import SparkContext
 from awsglue.job import Job
-
+ 
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
-
+ 
 job = Job(glueContext)
 job.init("bronze_job", {})
-
+ 
 metadata = {
     'tables': [
         {
@@ -240,44 +240,44 @@ metadata = {
         'write_mode': 'overwrite'
     }
 }
-
+ 
 runtime_config = metadata.get('runtime_config', {})
 base_path = runtime_config.get('base_path')
 target_path = runtime_config.get('target_path')
 read_format = runtime_config.get('read_format')
 write_format = runtime_config.get('write_format')
 write_mode = runtime_config.get('write_mode')
-
+ 
 for table_meta in metadata.get('tables', []):
     mapping_details = table_meta.get('mapping_details', '')
     mapping_parts = mapping_details.split()
     source_table = mapping_parts[0] if len(mapping_parts) > 0 else None
     source_alias = mapping_parts[1] if len(mapping_parts) > 1 else None
-
+ 
     target_table = table_meta.get('target_table')
     target_alias = table_meta.get('target_alias')
-
+ 
     reader = spark.read.format(read_format)
     if read_format == 'csv':
         reader = reader.option('header', 'true').option('inferSchema', 'true')
-
+ 
     df = reader.load(f"{base_path}{source_table}.{read_format}")
     df = df.alias(source_alias)
-
+ 
     transformations = []
     for col_meta in metadata.get('columns', []):
-        if col_meta.get('target_table') == source_alias:
+        if col_meta.get('target_table') == source_alias and f"= {target_alias}." in col_meta.get('transformation', ''):
             transformation = col_meta.get('transformation', '')
             rhs = transformation.split('=', 1)[0].strip() if '=' in transformation else transformation.strip()
             target_column = col_meta.get('target_column')
             transformations.append(f"{rhs} as {target_column}")
-
+ 
     df = df.selectExpr(*transformations)
-
+ 
     writer = df.write.mode(write_mode).format(write_format)
     if write_format == 'csv':
         writer = writer.option('header', 'true')
-
+ 
     writer.save(f"{target_path}{target_table}.{write_format}")
-
+ 
 job.commit()
