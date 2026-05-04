51176065d784fad7dc42a544c281a56ec0132a4b
karthikeyan-dev3 <karthi.online03@gmail.com>
2026-05-04 18:04:15 +0530

    Add/Update /tmp/sdlc_downloads/gold_script.py

--- gold/gold_script.py
+++ gold/gold_script.py
@@ -1,78 +1,60 @@
 ```python
-import sys
-from awsglue.transforms import *
-from awsglue.utils import getResolvedOptions
-from pyspark.context import SparkContext
 from awsglue.context import GlueContext
-from awsglue.job import Job
+from pyspark.context import SparkContext
 from pyspark.sql import SparkSession
-from awsglue.dynamicframe import DynamicFrame
 
-# Initialize Spark and Glue context
 sc = SparkContext()
 glueContext = GlueContext(sc)
-spark = SparkSession.builder.getOrCreate()
-job = Job(glueContext)
+spark = glueContext.spark_session
 
-# Set paths
-SOURCE_PATH = "s3://sdlc-agent-bucket/engineering-agent/silver/"
-TARGET_PATH = "s3://sdlc-agent-bucket/engineering-agent/gold/"
-FILE_FORMAT = "csv"
+# Read source table: iot_telemetry_data_silver
+iot_df = spark.read.format("csv").option("header", "true").load(f"s3://sdlc-agent-bucket/engineering-agent/silver/iot_telemetry_data_silver.csv/")
+iot_df.createOrReplaceTempView("iot_telemetry_data_silver")
 
-# Read and process for gold_sales
-sales_df = spark.read.format("csv").option("header", "true").load(f"{SOURCE_PATH}/sales_silver.csv/")
-sales_df.createOrReplaceTempView("ss")
+# Transform and write target table: gold_telemetry_data
+gold_telemetry_data = spark.sql("""
+    SELECT
+        device_id,
+        timestamp,
+        temperature,
+        humidity,
+        battery_level
+    FROM iot_telemetry_data_silver
+""")
+gold_telemetry_data.write.mode('overwrite').option("header", "true").csv("s3://sdlc-agent-bucket/engineering-agent/gold/gold_telemetry_data.csv")
 
-gold_sales_sql = """
-SELECT
-    CAST(transaction_id AS STRING) AS transaction_id,
-    CAST(store_id AS STRING) AS store_id,
-    CAST(sku_id AS STRING) AS sku_id,
-    CAST(quantity_sold AS INTEGER) AS quantity_sold,
-    CAST(transaction_date AS STRING) AS transaction_date,
-    CAST(price_per_unit AS DECIMAL(18,2)) AS price_per_unit,
-    CAST(sales_revenue AS DECIMAL(18,2)) AS sales_revenue
-FROM ss
-"""
+# Read source table: drug_stability_thresholds_silver
+drug_stability_df = spark.read.format("csv").option("header", "true").load(f"s3://sdlc-agent-bucket/engineering-agent/silver/drug_stability_thresholds_silver.csv/")
+drug_stability_df.createOrReplaceTempView("drug_stability_thresholds_silver")
 
-gold_sales_df = spark.sql(gold_sales_sql)
-gold_sales_df.coalesce(1).write.mode("overwrite").option("header", "true").csv(f"{TARGET_PATH}/gold_sales.csv")
+# Transform and write target table: gold_drug_stability_thresholds
+gold_drug_stability_thresholds = spark.sql("""
+    SELECT
+        drug_id,
+        temperature_threshold,
+        humidity_threshold,
+        min_battery_level
+    FROM drug_stability_thresholds_silver
+""")
+gold_drug_stability_thresholds.write.mode('overwrite').option("header", "true").csv("s3://sdlc-agent-bucket/engineering-agent/gold/gold_drug_stability_thresholds.csv")
 
-# Read and process for gold_inventory
-inventory_df = spark.read.format("csv").option("header", "true").load(f"{SOURCE_PATH}/inventory_silver.csv/")
-inventory_df.createOrReplaceTempView("is")
+# Read source table: telemetry_compliance_insights_silver
+compliance_insights_df = spark.read.format("csv").option("header", "true").load(f"s3://sdlc-agent-bucket/engineering-agent/silver/telemetry_compliance_insights_silver.csv/")
+compliance_insights_df.createOrReplaceTempView("telemetry_compliance_insights_silver")
 
-demand_forecast_df = spark.read.format("csv").option("header", "true").load(f"{TARGET_PATH}/gold_demand_forecast_metrics.csv/")
-demand_forecast_df.createOrReplaceTempView("gdfm")
-
-gold_inventory_sql = """
-SELECT
-    is.store_id,
-    is.sku_id,
-    is.stock_on_hand,
-    COALESCE(gdfm.demand_forecast, 0) AS demand_forecast
-FROM is
-LEFT JOIN gdfm
-ON is.store_id = gdfm.store_id AND is.sku_id = gdfm.sku_id
-"""
-
-gold_inventory_df = spark.sql(gold_inventory_sql)
-gold_inventory_df.coalesce(1).write.mode("overwrite").option("header", "true").csv(f"{TARGET_PATH}/gold_inventory.csv")
-
-# Read and process for gold_demand_forecast_metrics
-gold_demand_forecast_metrics_sql = """
-SELECT
-    ss.store_id,
-    ss.sku_id,
-    DATE(ss.transaction_date) AS forecast_date,
-    SUM(ss.quantity_sold) AS actual_quantity_sold
-FROM ss
-GROUP BY ss.store_id, ss.sku_id, DATE(ss.transaction_date)
-"""
-
-gold_demand_forecast_metrics_df = spark.sql(gold_demand_forecast_metrics_sql)
-gold_demand_forecast_metrics_df.coalesce(1).write.mode("overwrite").option("header", "true").csv(f"{TARGET_PATH}/gold_demand_forecast_metrics.csv")
-
-# Finish job
-job.commit()
+# Transform and write target table: gold_insights
+gold_insights = spark.sql("""
+    SELECT
+        device_id,
+        timestamp,
+        temperature,
+        humidity,
+        battery_level,
+        temperature_threshold_violation,
+        humidity_threshold_violation,
+        battery_level_violation,
+        compliance_status
+    FROM telemetry_compliance_insights_silver
+""")
+gold_insights.write.mode('overwrite').option("header", "true").csv("s3://sdlc-agent-bucket/engineering-agent/gold/gold_insights.csv")
 ```
