c40b0791741ff58892e5ae79968a23c5d4fbc5c2
karthikeyan-dev3 <karthi.online03@gmail.com>
2026-05-08 13:24:05 +0530

    Add/Update /tmp/sdlc_downloads/silver_script.py

--- silver/silver_script.py
+++ silver/silver_script.py
@@ -1,89 +1,102 @@
 import sys
-from pyspark.context import SparkContext
-from awsglue.context import GlueContext
 from awsglue.utils import getResolvedOptions
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from pyspark.context import SparkContext
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, row_number, trim, upper, lower, coalesce, when, date_format
-from pyspark.sql.window import Window
 
-args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+args = getResolvedOptions(sys.argv, ["JOB_NAME"])
 
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args["JOB_NAME"], args)
 
-# Source paths
-source_path = "s3://sdlc-agent-bucket/engineering-agent/bronze/"
+SOURCE_PATH = "s3://sdlc-agent-bucket/engineering-agent/bronze/"
+TARGET_PATH = "s3://sdlc-agent-bucket/engineering-agent/silver/"
+FILE_FORMAT = "csv"
 
-# Target path
-target_path = "s3://sdlc-agent-bucket/engineering-agent/silver/"
+# ----------------------------
+# Read source tables from S3
+# ----------------------------
+sales_transactions_bronze_df = (
+    spark.read.format(FILE_FORMAT)
+    .option("header", "true")
+    .option("inferSchema", "true")
+    .load(f"{SOURCE_PATH}/sales_transactions_bronze.{FILE_FORMAT}/")
+)
 
-# Read sales_bronze
-sales_df = spark.read.format("csv").option("header", "true").load(f"{source_path}/sales_bronze.csv/")
-sales_df.createOrReplaceTempView("sales_bronze")
+products_bronze_df = (
+    spark.read.format(FILE_FORMAT)
+    .option("header", "true")
+    .option("inferSchema", "true")
+    .load(f"{SOURCE_PATH}/products_bronze.{FILE_FORMAT}/")
+)
 
-# Transform sales_silver
-sales_query = """
-SELECT 
-    sb.transaction_id,
-    sb.store_id,
-    sb.product_id,
-    sb.quantity,
-    sb.sale_amount,
-    CAST(sb.transaction_time AS DATE) AS sale_date
-FROM (
-    SELECT *,
-           ROW_NUMBER() OVER (PARTITION BY transaction_id, store_id, product_id, transaction_time 
-                              ORDER BY transaction_time DESC) AS rn
-    FROM sales_bronze sb
-    WHERE 
-        sb.transaction_id IS NOT NULL 
-        AND sb.store_id IS NOT NULL 
-        AND sb.product_id IS NOT NULL
-        AND sb.quantity > 0 
-        AND sb.sale_amount > 0
-) sb
-WHERE sb.rn = 1
-"""
-sales_transformed = spark.sql(sales_query)
-sales_transformed.write.mode("overwrite").csv(f"{target_path}/sales_silver.csv", header=True)
+stores_bronze_df = (
+    spark.read.format(FILE_FORMAT)
+    .option("header", "true")
+    .option("inferSchema", "true")
+    .load(f"{SOURCE_PATH}/stores_bronze.{FILE_FORMAT}/")
+)
 
-# Read products_bronze
-products_df = spark.read.format("csv").option("header", "true").load(f"{source_path}/products_bronze.csv/")
-products_df.createOrReplaceTempView("products_bronze")
+# ----------------------------
+# Create temp views
+# ----------------------------
+sales_transactions_bronze_df.createOrReplaceTempView("sales_transactions_bronze")
+products_bronze_df.createOrReplaceTempView("products_bronze")
+stores_bronze_df.createOrReplaceTempView("stores_bronze")
 
-# Transform products_silver
-products_query = """
+spark.sql("CREATE OR REPLACE TEMP VIEW bronze_sales_transactions_bronze AS SELECT * FROM sales_transactions_bronze")
+spark.sql("CREATE OR REPLACE TEMP VIEW bronze_products_bronze AS SELECT * FROM products_bronze")
+spark.sql("CREATE OR REPLACE TEMP VIEW bronze_stores_bronze AS SELECT * FROM stores_bronze")
+
+# ----------------------------
+# Transform: sales_transactions_silver
+# ----------------------------
+sales_transactions_silver_df = spark.sql("""
+WITH base AS (
+    SELECT
+        stb.transaction_id,
+        stb.store_id,
+        stb.product_id,
+        CAST(stb.transaction_time AS DATE) AS transaction_date,
+        stb.quantity AS quantity_sold,
+        stb.sale_amount AS total_revenue,
+        CASE WHEN stb.quantity > 0 THEN stb.sale_amount / stb.quantity END AS price_per_unit,
+        ROW_NUMBER() OVER (
+            PARTITION BY stb.transaction_id, stb.product_id, stb.store_id, stb.transaction_time
+            ORDER BY stb.transaction_time DESC
+        ) AS rn
+    FROM bronze_sales_transactions_bronze stb
+    INNER JOIN bronze_products_bronze pb
+        ON stb.product_id = pb.product_id
+       AND pb.is_active = true
+    INNER JOIN bronze_stores_bronze sb
+        ON stb.store_id = sb.store_id
+)
 SELECT
-    pb.product_id,
-    TRIM(UPPER(pb.product_name)) AS product_name,
-    pb.is_active
-FROM (
-    SELECT *,
-           ROW_NUMBER() OVER (PARTITION BY product_id ORDER BY is_active DESC) AS rn
-    FROM products_bronze pb
-) pb
-WHERE pb.rn = 1
-"""
-products_transformed = spark.sql(products_query)
-products_transformed.write.mode("overwrite").csv(f"{target_path}/products_silver.csv", header=True)
+    transaction_id,
+    store_id,
+    product_id,
+    transaction_date,
+    quantity_sold,
+    total_revenue,
+    price_per_unit
+FROM base
+WHERE rn = 1
+""")
 
-# Read stores_bronze
-stores_df = spark.read.format("csv").option("header", "true").load(f"{source_path}/stores_bronze.csv/")
-stores_df.createOrReplaceTempView("stores_bronze")
+# ----------------------------
+# Write target table as SINGLE CSV file directly under TARGET_PATH
+# ----------------------------
+(
+    sales_transactions_silver_df.coalesce(1)
+    .write.mode("overwrite")
+    .format("csv")
+    .option("header", "true")
+    .save(f"{TARGET_PATH}/sales_transactions_silver.csv")
+)
 
-# Transform stores_silver
-stores_query = """
-SELECT
-    stb.store_id,
-    TRIM(stb.store_name) AS store_name,
-    TRIM(UPPER(stb.state)) AS state
-FROM (
-    SELECT *,
-           ROW_NUMBER() OVER (PARTITION BY store_id ORDER BY CAST(open_date AS DATE) DESC) AS rn
-    FROM stores_bronze stb
-) stb
-WHERE stb.rn = 1
-"""
-stores_transformed = spark.sql(stores_query)
-stores_transformed.write.mode("overwrite").csv(f"{target_path}/stores_silver.csv", header=True)
+job.commit()
