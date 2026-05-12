3c83e6a9440da742d97f0667917a2610f7f57edc
karthikeyan-dev3 <karthi.online03@gmail.com>
2026-05-12 15:09:05 +0530

    Add/Update /tmp/sdlc_downloads/gold_script.py

--- gold/gold_script.py
+++ gold/gold_script.py
@@ -1,67 +1,72 @@
 import sys
-from pyspark.context import SparkContext
-from awsglue.context import GlueContext
 from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
-from pyspark.sql import SparkSession, DataFrame
-from pyspark.sql.functions import col, row_number
-from pyspark.sql.window import Window
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from pyspark.sql import SparkSession
 
-# Initialize Spark and Glue Contexts
-sc = SparkContext()
-glueContext = GlueContext(sc)
+# Initialize GlueContext and SparkSession
+glueContext = GlueContext(SparkContext.getOrCreate())
 spark = glueContext.spark_session
 
+# Job parameters
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
 # Paths
-SOURCE_PATH = "s3://sdlc-agent-bucket/engineering-agent/silver/"
-TARGET_PATH = "s3://sdlc-agent-bucket/engineering-agent/gold/"
+SOURCE_PATH = "s3://sdlc-agent-bucket/engineering-agent/silver"
+TARGET_PATH = "s3://sdlc-agent-bucket/engineering-agent/gold"
 FILE_FORMAT = "csv"
 
-# Read source tables from S3
-departments_silver_df = spark.read.format(FILE_FORMAT).option("header", "true").load(f"{SOURCE_PATH}/departments_silver.{FILE_FORMAT}/")
-dates_silver_df = spark.read.format(FILE_FORMAT).option("header", "true").load(f"{SOURCE_PATH}/dates_silver.{FILE_FORMAT}/")
-certifications_silver_df = spark.read.format(FILE_FORMAT).option("header", "true").load(f"{SOURCE_PATH}/certifications_silver.{FILE_FORMAT}/")
-employees_silver_df = spark.read.format(FILE_FORMAT).option("header", "true").load(f"{SOURCE_PATH}/employees_silver.{FILE_FORMAT}/")
+# Load data
+certification_tracking_silver = (
+    spark.read
+    .format(FILE_FORMAT)
+    .option("header", "true")
+    .load(f"{SOURCE_PATH}/certification_tracking_silver.{FILE_FORMAT}/")
+)
 
-# Create temp views
-departments_silver_df.createOrReplaceTempView("ds")
-dates_silver_df.createOrReplaceTempView("dts")
-certifications_silver_df.createOrReplaceTempView("cs")
-employees_silver_df.createOrReplaceTempView("es")
+# Create temp view
+certification_tracking_silver.createOrReplaceTempView("cts")
 
-# Transform: gold_dim_departments
-gold_dim_departments_df = spark.sql("""
-    SELECT 
-        ds.department_id AS department_id,
-        TRIM(ds.department_name) AS department_name,
-        UPPER(ds.normalized_department_code) AS normalized_department_code
-    FROM ds
-    """)
-gold_dim_departments_df.write.mode("overwrite").option("header", "true").csv(f"{TARGET_PATH}/gold_dim_departments.csv")
+# Transform and write gold_certification_tracking
+gold_certification_tracking_df = spark.sql("""
+SELECT
+    cts.department_id,
+    cts.department_name,
+    cts.coordinator_id,
+    cts.coordinator_name,
+    cts.certification_id,
+    cts.certification_name,
+    cts.completion_status,
+    cts.completion_date,
+    cts.compliance_deadline,
+    cts.flagged_for_incomplete
+FROM
+    cts
+""")
 
-# Transform: gold_dim_dates
-gold_dim_dates_df = spark.sql("""
-    SELECT 
-        dts.date_id AS date_id,
-        dts.date AS date,
-        dts.fiscal_year AS fiscal_year,
-        dts.fiscal_period AS fiscal_period
-    FROM dts
-    """)
-gold_dim_dates_df.write.mode("overwrite").option("header", "true").csv(f"{TARGET_PATH}/gold_dim_dates.csv")
+gold_certification_tracking_df.coalesce(1).write.csv(f"{TARGET_PATH}/gold_certification_tracking.csv", header=True, mode="overwrite")
 
-# Transform: gold_fact_certifications
-gold_fact_certifications_df = spark.sql("""
-    SELECT 
-        cs.certification_id AS certification_id,
-        cs.employee_id AS employee_id,
-        TRIM(cs.certification_name) AS certification_name,
-        ds.department_id AS department_id,
-        cs.completion_date AS completion_date,
-        cs.deadline_date AS deadline_date,
-        CASE WHEN cs.completion_date <= cs.deadline_date THEN TRUE ELSE FALSE END AS is_compliant
-    FROM cs
-    INNER JOIN es ON cs.employee_id = es.employee_id
-    INNER JOIN ds ON es.department_id = ds.department_id
-    """)
-gold_fact_certifications_df.write.mode("overwrite").option("header", "true").csv(f"{TARGET_PATH}/gold_fact_certifications.csv")
+# Transform and write gold_department_compliance_report
+gold_department_compliance_report_df = spark.sql("""
+SELECT
+    cts.department_id,
+    cts.department_name,
+    COUNT(cts.certification_id) AS total_certifications,
+    SUM(CASE WHEN cts.completion_status = 'COMPLETED' THEN 1 ELSE 0 END) AS completed_certifications,
+    SUM(CASE WHEN cts.flagged_for_incomplete = true THEN 1 ELSE 0 END) AS non_compliant_certifications,
+    (SUM(CASE WHEN cts.completion_status = 'COMPLETED' THEN 1 ELSE 0 END) / COUNT(cts.certification_id)) AS compliance_rate,
+    CURRENT_TIMESTAMP AS last_updated
+FROM
+    cts
+GROUP BY
+    cts.department_id,
+    cts.department_name
+""")
+
+gold_department_compliance_report_df.coalesce(1).write.csv(f"{TARGET_PATH}/gold_department_compliance_report.csv", header=True, mode="overwrite")
+
+job.commit()
