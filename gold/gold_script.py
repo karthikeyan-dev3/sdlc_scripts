fa1e8a58d0b3c60ef29348c27bb71dc68fd499d4
karthikeyan-dev3 <karthi.online03@gmail.com>
2026-05-13 14:12:21 +0530

    Add/Update /tmp/sdlc_downloads/gold_script.py

--- gold/gold_script.py
+++ gold/gold_script.py
@@ -1,89 +1,88 @@
-import sys
-from awsglue.transforms import *
+```python
+from awsglue.context import GlueContext
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
-from awsglue.context import GlueContext
-from awsglue.job import Job
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, row_number
-from pyspark.sql.window import Window
 
 # Initialize GlueContext and SparkSession
-args = getResolvedOptions(sys.argv, ['JOB_NAME'])
-sc = SparkContext()
+sc = SparkContext.getOrCreate()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
-job = Job(glueContext)
-job.init(args['JOB_NAME'], args)
 
-# Set paths
+# Define source and target paths
 SOURCE_PATH = "s3://sdlc-agent-bucket/engineering-agent/silver/"
 TARGET_PATH = "s3://sdlc-agent-bucket/engineering-agent/gold/"
 FILE_FORMAT = "csv"
 
-# Process gold_inventory_usage
-inventory_usage_df = (
-    spark.read
-    .format(FILE_FORMAT)
-    .option("header", "true")
-    .load(f"{SOURCE_PATH}/inventory_usage_silver.{FILE_FORMAT}/")
-)
+# Process for gold_sales
+sales_transactions_df = spark.read \
+    .format(FILE_FORMAT) \
+    .option("header", "true") \
+    .load(f"{SOURCE_PATH}/sales_transactions_silver.{FILE_FORMAT}/")
 
-inventory_usage_df.createOrReplaceTempView("ius")
+sales_transactions_df.createOrReplaceTempView("sales_transactions_silver")
 
-gold_inventory_usage_df = spark.sql("""
-    SELECT 
-        ius.ingredient_id,
-        ius.date,
-        ius.quantity_used,
-        COALESCE(ius.waste_quantity, 0.0) as waste_quantity,
-        ius.location_id
-    FROM ius
-""")
+gold_sales_sql = """
+SELECT
+    CAST(sales_id AS STRING) AS sales_id,
+    CAST(product_id AS STRING) AS product_id,
+    CAST(store_id AS STRING) AS store_id,
+    CAST(transaction_date AS DATE) AS transaction_date,
+    CAST(quantity_sold AS INT) AS quantity_sold,
+    CAST(sales_amount AS DOUBLE) AS sales_amount
+FROM sales_transactions_silver
+"""
 
-gold_inventory_usage_df.write.mode("overwrite").csv(f"{TARGET_PATH}/gold_inventory_usage.csv", header=True)
+gold_sales_df = spark.sql(gold_sales_sql)
 
-# Process gold_supplier_performance
-supplier_performance_df = (
-    spark.read
-    .format(FILE_FORMAT)
-    .option("header", "true")
-    .load(f"{SOURCE_PATH}/supplier_performance_silver.{FILE_FORMAT}/")
-)
+gold_sales_df.coalesce(1).write \
+    .option("header", "true") \
+    .csv(f"{TARGET_PATH}/gold_sales.csv", mode='overwrite')
 
-supplier_performance_df.createOrReplaceTempView("sps")
+# Process for gold_product
+products_df = spark.read \
+    .format(FILE_FORMAT) \
+    .option("header", "true") \
+    .load(f"{SOURCE_PATH}/products_silver.{FILE_FORMAT}/")
 
-gold_supplier_performance_df = spark.sql("""
-    SELECT 
-        sps.supplier_id,
-        sps.delivery_date,
-        sps.quantity_delivered,
-        sps.on_time_delivery
-    FROM sps
-""")
+products_df.createOrReplaceTempView("products_silver")
 
-gold_supplier_performance_df.write.mode("overwrite").csv(f"{TARGET_PATH}/gold_supplier_performance.csv", header=True)
+gold_product_sql = """
+SELECT
+    CAST(product_id AS STRING) AS product_id,
+    TRIM(product_name) AS product_name,
+    TRIM(category) AS category,
+    TRIM(brand) AS brand,
+    CAST(list_price AS FLOAT) AS list_price
+FROM products_silver
+"""
 
-# Process gold_spoilage_trends
-spoilage_trends_df = (
-    spark.read
-    .format(FILE_FORMAT)
-    .option("header", "true")
-    .load(f"{SOURCE_PATH}/spoilage_trends_silver.{FILE_FORMAT}/")
-)
+gold_product_df = spark.sql(gold_product_sql)
 
-spoilage_trends_df.createOrReplaceTempView("sts")
+gold_product_df.coalesce(1).write \
+    .option("header", "true") \
+    .csv(f"{TARGET_PATH}/gold_product.csv", mode='overwrite')
 
-gold_spoilage_trends_df = spark.sql("""
-    SELECT 
-        sts.ingredient_id,
-        sts.spoilage_date,
-        sts.waste_quantity,
-        sts.reason_for_spoilage
-    FROM sts
-""")
+# Process for gold_store
+stores_df = spark.read \
+    .format(FILE_FORMAT) \
+    .option("header", "true") \
+    .load(f"{SOURCE_PATH}/stores_silver.{FILE_FORMAT}/")
 
-gold_spoilage_trends_df.write.mode("overwrite").csv(f"{TARGET_PATH}/gold_spoilage_trends.csv", header=True)
+stores_df.createOrReplaceTempView("stores_silver")
 
-# Commit the job
-job.commit()
+gold_store_sql = """
+SELECT
+    CAST(store_id AS STRING) AS store_id,
+    TRIM(store_name) AS store_name,
+    TRIM(location) AS location,
+    TRIM(store_type) AS store_type
+FROM stores_silver
+"""
+
+gold_store_df = spark.sql(gold_store_sql)
+
+gold_store_df.coalesce(1).write \
+    .option("header", "true") \
+    .csv(f"{TARGET_PATH}/gold_store.csv", mode='overwrite')
+```
