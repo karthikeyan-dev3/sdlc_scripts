eae32b44e88717a373e78826d7c688854d13fb29
karthikeyan-dev3 <karthi.online03@gmail.com>
2026-05-19 13:45:58 +0530

    Add/Update /tmp/sdlc_downloads/gold_script.py

--- gold/gold_script.py
+++ gold/gold_script.py
@@ -1,15 +1,11 @@
 import sys
-from awsglue.utils import getResolvedOptions
 from awsglue.context import GlueContext
 from awsglue.job import Job
+from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from pyspark.sql import SparkSession
 
 args = getResolvedOptions(sys.argv, ["JOB_NAME"])
-
-SOURCE_PATH = "s3://sdlc-agent-bucket/engineering-agent/silver/"
-TARGET_PATH = "s3://sdlc-agent-bucket/engineering-agent/gold/"
-FILE_FORMAT = "csv"
 
 sc = SparkContext()
 glueContext = GlueContext(sc)
@@ -17,138 +13,132 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args["JOB_NAME"], args)
 
-# -------------------------------------------------------------------
-# Read source tables from S3 + create temp views
-# -------------------------------------------------------------------
+SOURCE_PATH = "s3://sdlc-agent-bucket/engineering-agent/silver/"
+TARGET_PATH = "s3://sdlc-agent-bucket/engineering-agent/gold/"
+FILE_FORMAT = "csv"
 
-clinical_trials_silver_df = (
+# ------------------------------------------------------------
+# 1) Read source tables from S3
+# ------------------------------------------------------------
+sts_df = (
     spark.read.format(FILE_FORMAT)
     .option("header", "true")
-    .load(f"{SOURCE_PATH}/clinical_trials_silver.{FILE_FORMAT}/")
+    .load(f"{SOURCE_PATH}/sales_transactions_silver.{FILE_FORMAT}/")
 )
-clinical_trials_silver_df.createOrReplaceTempView("clinical_trials_silver")
 
-audit_trail_silver_df = (
+ps_df = (
     spark.read.format(FILE_FORMAT)
     .option("header", "true")
-    .load(f"{SOURCE_PATH}/audit_trail_silver.{FILE_FORMAT}/")
+    .load(f"{SOURCE_PATH}/products_silver.{FILE_FORMAT}/")
 )
-audit_trail_silver_df.createOrReplaceTempView("audit_trail_silver")
 
-data_lineage_silver_df = (
+ss_df = (
     spark.read.format(FILE_FORMAT)
     .option("header", "true")
-    .load(f"{SOURCE_PATH}/data_lineage_silver.{FILE_FORMAT}/")
-)
-data_lineage_silver_df.createOrReplaceTempView("data_lineage_silver")
-
-historical_versions_silver_df = (
-    spark.read.format(FILE_FORMAT)
-    .option("header", "true")
-    .load(f"{SOURCE_PATH}/historical_versions_silver.{FILE_FORMAT}/")
-)
-historical_versions_silver_df.createOrReplaceTempView("historical_versions_silver")
-
-# -------------------------------------------------------------------
-# Target: gold_clinical_trials
-# -------------------------------------------------------------------
-
-gold_clinical_trials_df = spark.sql(
-    """
-    SELECT
-        cts.trial_id AS trial_id,
-        CAST(cts.start_date AS DATE) AS start_date,
-        cts.trial_name AS trial_name,
-        cts.study_phase AS study_phase,
-        cts.end_date AS end_date,
-        cts.principal_investigator AS principal_investigator,
-        cts.sponsor AS sponsor,
-        cts.compliance_status AS compliance_status
-    FROM clinical_trials_silver cts
-    """
+    .load(f"{SOURCE_PATH}/stores_silver.{FILE_FORMAT}/")
 )
 
+# ------------------------------------------------------------
+# 2) Create temp views
+# ------------------------------------------------------------
+sts_df.createOrReplaceTempView("sales_transactions_silver")
+ps_df.createOrReplaceTempView("products_silver")
+ss_df.createOrReplaceTempView("stores_silver")
+
+# ------------------------------------------------------------
+# 3) Transformations using Spark SQL (per target table)
+# ------------------------------------------------------------
+
+# Target: gold.gold_sales
+gold_sales_df = spark.sql("""
+SELECT
+  CAST(sts.transaction_id AS STRING) AS transaction_id,
+  CAST(sts.store_id AS STRING) AS store_id,
+  CAST(sts.product_id AS STRING) AS product_id,
+  CAST(sts.date AS DATE) AS date,
+  CAST(sts.quantity_sold AS INT) AS quantity_sold,
+  CAST(sts.total_revenue AS DOUBLE) AS total_revenue
+FROM sales_transactions_silver sts
+""")
+
+# Target: gold.gold_product_performance
+gold_product_performance_df = spark.sql("""
+SELECT
+  CAST(sts.product_id AS STRING) AS product_id,
+  CAST(ps.product_name AS STRING) AS product_name,
+  CAST(ps.category AS STRING) AS category,
+  CAST(SUM(CAST(sts.total_revenue AS DOUBLE)) AS DOUBLE) AS total_revenue,
+  CAST(SUM(CAST(sts.quantity_sold AS INT)) AS INT) AS quantity_sold
+FROM sales_transactions_silver sts
+INNER JOIN products_silver ps
+  ON sts.product_id = ps.product_id
+GROUP BY
+  sts.product_id,
+  ps.product_name,
+  ps.category
+""")
+
+# Target: gold.gold_store_performance
+gold_store_performance_df = spark.sql("""
+SELECT
+  CAST(sts.store_id AS STRING) AS store_id,
+  CAST(ss.store_name AS STRING) AS store_name,
+  CAST(SUM(CAST(sts.total_revenue AS DOUBLE)) AS DOUBLE) AS total_revenue,
+  CAST(COUNT(DISTINCT sts.transaction_id) AS STRING) AS total_transactions
+FROM sales_transactions_silver sts
+INNER JOIN stores_silver ss
+  ON sts.store_id = ss.store_id
+GROUP BY
+  sts.store_id,
+  ss.store_name
+""")
+
+# Target: gold.gold_category_performance
+gold_category_performance_df = spark.sql("""
+SELECT
+  CAST(ps.category AS STRING) AS category,
+  CAST(SUM(CAST(sts.total_revenue AS DOUBLE)) AS DOUBLE) AS total_revenue,
+  CAST(SUM(CAST(sts.quantity_sold AS INT)) AS INT) AS quantity_sold
+FROM sales_transactions_silver sts
+INNER JOIN products_silver ps
+  ON sts.product_id = ps.product_id
+GROUP BY
+  ps.category
+""")
+
+# ------------------------------------------------------------
+# 4) Save output (single CSV file directly under TARGET_PATH)
+# ------------------------------------------------------------
 (
-    gold_clinical_trials_df.coalesce(1)
+    gold_sales_df.coalesce(1)
     .write.mode("overwrite")
     .format("csv")
     .option("header", "true")
-    .save(f"{TARGET_PATH}/gold_clinical_trials.csv")
-)
-
-# -------------------------------------------------------------------
-# Target: gold_audit_trail
-# -------------------------------------------------------------------
-
-gold_audit_trail_df = spark.sql(
-    """
-    SELECT
-        ats.record_id AS record_id,
-        CAST(ats.change_timestamp AS TIMESTAMP) AS change_timestamp,
-        ats.changed_by AS changed_by,
-        ats.field_name AS field_name,
-        ats.old_value AS old_value,
-        ats.new_value AS new_value,
-        ats.change_reason AS change_reason
-    FROM audit_trail_silver ats
-    """
+    .save(f"{TARGET_PATH}/gold_sales.csv")
 )
 
 (
-    gold_audit_trail_df.coalesce(1)
+    gold_product_performance_df.coalesce(1)
     .write.mode("overwrite")
     .format("csv")
     .option("header", "true")
-    .save(f"{TARGET_PATH}/gold_audit_trail.csv")
-)
-
-# -------------------------------------------------------------------
-# Target: gold_data_lineage
-# -------------------------------------------------------------------
-
-gold_data_lineage_df = spark.sql(
-    """
-    SELECT
-        dls.lineage_id AS lineage_id,
-        dls.source_system AS source_system,
-        dls.transformation_description AS transformation_description,
-        dls.target_table AS target_table,
-        dls.load_timestamp AS load_timestamp
-    FROM data_lineage_silver dls
-    """
+    .save(f"{TARGET_PATH}/gold_product_performance.csv")
 )
 
 (
-    gold_data_lineage_df.coalesce(1)
+    gold_store_performance_df.coalesce(1)
     .write.mode("overwrite")
     .format("csv")
     .option("header", "true")
-    .save(f"{TARGET_PATH}/gold_data_lineage.csv")
-)
-
-# -------------------------------------------------------------------
-# Target: gold_historical_versions
-# -------------------------------------------------------------------
-
-gold_historical_versions_df = spark.sql(
-    """
-    SELECT
-        hvs.version_id AS version_id,
-        hvs.record_id AS record_id,
-        hvs.version_number AS version_number,
-        CAST(hvs.version_start_date AS DATE) AS version_start_date,
-        hvs.version_end_date AS version_end_date,
-        hvs.data_snapshot AS data_snapshot
-    FROM historical_versions_silver hvs
-    """
+    .save(f"{TARGET_PATH}/gold_store_performance.csv")
 )
 
 (
-    gold_historical_versions_df.coalesce(1)
+    gold_category_performance_df.coalesce(1)
     .write.mode("overwrite")
     .format("csv")
     .option("header", "true")
-    .save(f"{TARGET_PATH}/gold_historical_versions.csv")
+    .save(f"{TARGET_PATH}/gold_category_performance.csv")
 )
 
 job.commit()
