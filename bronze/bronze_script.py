7ec638a2d5c9ff957bf8bd948d8279a39b7f3ef7
karthikeyan-dev3 <karthi.online03@gmail.com>
2026-05-19 12:39:22 +0530

    Add/Update /tmp/sdlc_downloads/bronze_script.py

--- bronze/bronze_script.py
+++ bronze/bronze_script.py
@@ -1,262 +1,89 @@
-from awsglue.context import GlueContext
+<GLUE_SCRIPT>
+import sys
+import os
+import logging
+import json
+import datetime
+
+from pyspark.sql import functions as F
+from pyspark.sql import types
 from pyspark.context import SparkContext
+
+from awsglue.context import GlueContext
 from awsglue.job import Job
+from awsglue.utils import getResolvedOptions
 
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s | %(levelname)s | %(message)s")
+logger = logging.getLogger(__name__)
+
+args = getResolvedOptions(sys.argv, ['JOB_NAME', 'METADATA'])
+meta = json.loads(args['METADATA'])
 sc = SparkContext()
-glueContext = GlueContext(sc)
-spark = glueContext.spark_session
+gc = GlueContext(sc)
+spark = gc.spark_session
+job = Job(gc)
+job.init(args['JOB_NAME'], args)
 
-job = Job(glueContext)
-job.init("bronze_job", {})
 
-metadata = {
-    'tables': [
-        {
-            'target_schema': 'bronze',
-            'target_table': 'products_bronze',
-            'target_alias': 'pb',
-            'mapping_details': 'products_raw pr',
-            'description': 'Bronze ingestion of product master data from products_raw with no joins or aggregations. Includes product_id, product_name, category, brand, price, is_active.'
-        },
-        {
-            'target_schema': 'bronze',
-            'target_table': 'sales_transactions_bronze',
-            'target_alias': 'stb',
-            'mapping_details': 'sales_transactions_raw str',
-            'description': 'Bronze ingestion of sales transaction facts from sales_transactions_raw with no joins or aggregations. Includes transaction_id, store_id, product_id, quantity, sale_amount, transaction_time.'
-        },
-        {
-            'target_schema': 'bronze',
-            'target_table': 'stores_bronze',
-            'target_alias': 'sb',
-            'mapping_details': 'stores_raw sr',
-            'description': 'Bronze ingestion of store master data from stores_raw with no joins or aggregations. Includes store_id, store_name, city, state, store_type, open_date.'
-        }
-    ],
-    'columns': [
-        {
-            'source_column': "['pr.product_id']",
-            'source_type': 'varchar(10)',
-            'source_nullable': 'not specified',
-            'target_column': 'product_id',
-            'target_type': 'varchar(10)',
-            'target_nullable': 'not specified',
-            'transformation': 'pr.product_id = pr.product_id',
-            'target_table': 'pr'
-        },
-        {
-            'source_column': "['pr.product_name']",
-            'source_type': 'varchar(255)',
-            'source_nullable': 'not specified',
-            'target_column': 'product_name',
-            'target_type': 'varchar(255)',
-            'target_nullable': 'not specified',
-            'transformation': 'pr.product_name = pr.product_name',
-            'target_table': 'pr'
-        },
-        {
-            'source_column': "['pr.category']",
-            'source_type': 'varchar(100)',
-            'source_nullable': 'not specified',
-            'target_column': 'category',
-            'target_type': 'varchar(100)',
-            'target_nullable': 'not specified',
-            'transformation': 'pr.category = pr.category',
-            'target_table': 'pr'
-        },
-        {
-            'source_column': "['pr.brand']",
-            'source_type': 'varchar(100)',
-            'source_nullable': 'not specified',
-            'target_column': 'brand',
-            'target_type': 'varchar(100)',
-            'target_nullable': 'not specified',
-            'transformation': 'pr.brand = pr.brand',
-            'target_table': 'pr'
-        },
-        {
-            'source_column': "['pr.price']",
-            'source_type': 'float',
-            'source_nullable': 'not specified',
-            'target_column': 'price',
-            'target_type': 'float',
-            'target_nullable': 'not specified',
-            'transformation': 'pr.price = pr.price',
-            'target_table': 'pr'
-        },
-        {
-            'source_column': "['pr.is_active']",
-            'source_type': 'boolean',
-            'source_nullable': 'not specified',
-            'target_column': 'is_active',
-            'target_type': 'boolean',
-            'target_nullable': 'not specified',
-            'transformation': 'pr.is_active = pr.is_active',
-            'target_table': 'pr'
-        },
-        {
-            'source_column': "['str.transaction_id']",
-            'source_type': 'varchar(10)',
-            'source_nullable': 'not specified',
-            'target_column': 'transaction_id',
-            'target_type': 'varchar(10)',
-            'target_nullable': 'not specified',
-            'transformation': 'str.transaction_id = str.transaction_id',
-            'target_table': 'str'
-        },
-        {
-            'source_column': "['str.store_id']",
-            'source_type': 'varchar(10)',
-            'source_nullable': 'not specified',
-            'target_column': 'store_id',
-            'target_type': 'varchar(10)',
-            'target_nullable': 'not specified',
-            'transformation': 'str.store_id = str.store_id',
-            'target_table': 'str'
-        },
-        {
-            'source_column': "['str.product_id']",
-            'source_type': 'varchar(10)',
-            'source_nullable': 'not specified',
-            'target_column': 'product_id',
-            'target_type': 'varchar(10)',
-            'target_nullable': 'not specified',
-            'transformation': 'str.product_id = str.product_id',
-            'target_table': 'str'
-        },
-        {
-            'source_column': "['str.quantity']",
-            'source_type': 'int',
-            'source_nullable': 'not specified',
-            'target_column': 'quantity',
-            'target_type': 'int',
-            'target_nullable': 'not specified',
-            'transformation': 'str.quantity = str.quantity',
-            'target_table': 'str'
-        },
-        {
-            'source_column': "['str.sale_amount']",
-            'source_type': 'double',
-            'source_nullable': 'not specified',
-            'target_column': 'sale_amount',
-            'target_type': 'double',
-            'target_nullable': 'not specified',
-            'transformation': 'str.sale_amount = str.sale_amount',
-            'target_table': 'str'
-        },
-        {
-            'source_column': "['str.transaction_time']",
-            'source_type': 'timestamp',
-            'source_nullable': 'not specified',
-            'target_column': 'transaction_time',
-            'target_type': 'timestamp',
-            'target_nullable': 'not specified',
-            'transformation': 'str.transaction_time = str.transaction_time',
-            'target_table': 'str'
-        },
-        {
-            'source_column': "['sr.store_id']",
-            'source_type': 'varchar(10)',
-            'source_nullable': 'not specified',
-            'target_column': 'store_id',
-            'target_type': 'varchar(10)',
-            'target_nullable': 'not specified',
-            'transformation': 'sr.store_id = sr.store_id',
-            'target_table': 'sr'
-        },
-        {
-            'source_column': "['sr.store_name']",
-            'source_type': 'varchar(255)',
-            'source_nullable': 'not specified',
-            'target_column': 'store_name',
-            'target_type': 'varchar(255)',
-            'target_nullable': 'not specified',
-            'transformation': 'sr.store_name = sr.store_name',
-            'target_table': 'sr'
-        },
-        {
-            'source_column': "['sr.city']",
-            'source_type': 'varchar(100)',
-            'source_nullable': 'not specified',
-            'target_column': 'city',
-            'target_type': 'varchar(100)',
-            'target_nullable': 'not specified',
-            'transformation': 'sr.city = sr.city',
-            'target_table': 'sr'
-        },
-        {
-            'source_column': "['sr.state']",
-            'source_type': 'varchar(100)',
-            'source_nullable': 'not specified',
-            'target_column': 'state',
-            'target_type': 'varchar(100)',
-            'target_nullable': 'not specified',
-            'transformation': 'sr.state = sr.state',
-            'target_table': 'sr'
-        },
-        {
-            'source_column': "['sr.store_type']",
-            'source_type': 'varchar(50)',
-            'source_nullable': 'not specified',
-            'target_column': 'store_type',
-            'target_type': 'varchar(50)',
-            'target_nullable': 'not specified',
-            'transformation': 'sr.store_type = sr.store_type',
-            'target_table': 'sr'
-        },
-        {
-            'source_column': "['sr.open_date']",
-            'source_type': 'date',
-            'source_nullable': 'not specified',
-            'target_column': 'open_date',
-            'target_type': 'date',
-            'target_nullable': 'not specified',
-            'transformation': 'sr.open_date = sr.open_date',
-            'target_table': 'sr'
-        }
-    ],
-    'runtime_config': {
-        'base_path': 's3://sdlc-agent-bucket/engineering-agent/src/',
-        'target_path': 's3://sdlc-agent-bucket/engineering-agent/bronze/',
-        'read_format': 'csv',
-        'write_format': 'csv',
-        'write_mode': 'overwrite'
-    }
-}
+def add_audit_columns(df, batch_id, source_system, source_path, partition_columns):
+    df = (df
+          .withColumn('_ingestion_timestamp', F.current_timestamp())
+          .withColumn('_batch_id', F.lit(batch_id))
+          .withColumn('_source_system', F.lit(source_system))
+          .withColumn('_source_path', F.lit(source_path)))
 
-runtime_config = metadata['runtime_config']
-base_path = runtime_config['base_path']
-target_path = runtime_config['target_path']
-read_format = runtime_config['read_format']
-write_format = runtime_config['write_format']
-write_mode = runtime_config['write_mode']
+    if '_ingestion_date' in partition_columns:
+        df = df.withColumn('_ingestion_date', F.to_date(F.current_timestamp()).cast('string'))
 
-for table_meta in metadata['tables']:
-    mapping_details = table_meta['mapping_details']
-    source_table, source_alias = mapping_details.split()
-    target_table = table_meta['target_table']
-    target_alias = table_meta['target_alias']
+    return df
 
-    reader = spark.read.format(read_format)
-    if read_format == 'csv':
-        reader = reader.option("header", "true").option("inferSchema", "true")
 
-    df = reader.load(base_path + source_table + "." + read_format)
-    df = df.alias(source_alias)
+for table in meta['tables']:
+    try:
+        source_table = table['mapping_details']['source_table']
+        source_alias = table['mapping_details']['source_alias']
+        target_table = table['target_table']
+        partition_cols = table['partition_columns']
 
-    transformations = []
-    for col_meta in metadata['columns']:
-        if col_meta.get('target_table') == source_alias:
-            rhs = col_meta['transformation'].split('=', 1)[1].strip()
-            target_col = col_meta['target_column']
-            transformations.append(f"{rhs} as {target_col}")
+        source_path = meta['runtime_config']['base_path'] + "/" + source_table
+        target_out_path = meta['runtime_config']['target_path'] + "/" + target_table
 
-    df_out = df.selectExpr(*transformations)
+        df = (spark.read
+              .format(meta['runtime_config']['read_format'])
+              .options(**meta['runtime_config']['read_options'])
+              .load(source_path))
+        df = df.alias(source_alias)
+        logger.info(f"READ  | table={source_table} | path={source_path}")
 
-    writer = df_out.write.mode(write_mode).format(write_format)
-    if write_format == 'csv':
-        writer = writer.option("header", "true")
+        matched_columns = [c for c in table['columns'] if c['target_alias'] == source_alias]
+        if len(matched_columns) == 0:
+            raise ValueError(f"No columns matched source_alias for table: {target_table}")
 
-    writer.save(target_path + target_table + "." + write_format)
+        df = df.selectExpr(*[c['transformation'] for c in matched_columns])
+
+        df = add_audit_columns(
+            df,
+            batch_id=meta['job']['batch_id'],
+            source_system=meta['job']['source_system'],
+            source_path=source_path,
+            partition_columns=partition_cols
+        )
+
+        writer = (df.write
+                  .mode(meta['runtime_config']['write_mode'])
+                  .format(meta['runtime_config']['write_format'])
+                  .options(**meta['runtime_config']['write_options']))
+
+        if len(partition_cols) > 0:
+            writer = writer.partitionBy(*partition_cols)
+
+        writer.save(target_out_path)
+        logger.info(f"WRITE | table={target_table} | path={target_out_path}")
+
+    except Exception as e:
+        logger.error(f"FAILED | table={table.get('target_table')} | error={e}")
+        raise
 
 job.commit()
+</GLUE_SCRIPT>
