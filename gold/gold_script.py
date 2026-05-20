21a8cbd3eaee434018a5604399c4ef46ef7ab737
karthikeyan-dev3 <karthi.online03@gmail.com>
2026-05-20 22:17:23 +0530

    Add/Update /tmp/sdlc_downloads/gold_script.py

--- gold/gold_script.py
+++ gold/gold_script.py
@@ -7,7 +7,7 @@ from pyspark.sql import SparkSession
 
 args = getResolvedOptions(sys.argv, ["JOB_NAME"])
 
-sc = SparkContext()
+sc = SparkContext.getOrCreate()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
@@ -17,52 +17,70 @@ SOURCE_PATH = "s3://sdlc-agent-bucket/engineering-agent/silver/"
 TARGET_PATH = "s3://sdlc-agent-bucket/engineering-agent/gold/"
 FILE_FORMAT = "csv"
 
-# ---------------------------
-# Read Source Tables (S3)
-# ---------------------------
-
-clinical_trial_sites_silver_df = (
+# ------------------------------------------------------------
+# Read source tables from S3
+# ------------------------------------------------------------
+tsdk_df = (
     spark.read.format(FILE_FORMAT)
     .option("header", "true")
-    .load(f"{SOURCE_PATH}/clinical_trial_sites_silver.{FILE_FORMAT}/")
+    .load(f"{SOURCE_PATH}/trial_site_daily_kpis_silver.{FILE_FORMAT}/")
 )
-clinical_trial_sites_silver_df.createOrReplaceTempView("clinical_trial_sites_silver")
 
-performance_trends_silver_df = (
+tspf_df = (
     spark.read.format(FILE_FORMAT)
     .option("header", "true")
-    .load(f"{SOURCE_PATH}/performance_trends_silver.{FILE_FORMAT}/")
+    .load(f"{SOURCE_PATH}/trial_site_performance_flags_silver.{FILE_FORMAT}/")
 )
-performance_trends_silver_df.createOrReplaceTempView("performance_trends_silver")
 
-trial_enrollment_analysis_silver_df = (
+tsmt_df = (
     spark.read.format(FILE_FORMAT)
     .option("header", "true")
-    .load(f"{SOURCE_PATH}/trial_enrollment_analysis_silver.{FILE_FORMAT}/")
+    .load(f"{SOURCE_PATH}/trial_site_metric_trends_silver.{FILE_FORMAT}/")
 )
-trial_enrollment_analysis_silver_df.createOrReplaceTempView("trial_enrollment_analysis_silver")
 
-data_quality_checks_silver_df = (
+dfqs_df = (
     spark.read.format(FILE_FORMAT)
     .option("header", "true")
-    .load(f"{SOURCE_PATH}/data_quality_checks_silver.{FILE_FORMAT}/")
+    .load(f"{SOURCE_PATH}/data_freshness_quality_silver.{FILE_FORMAT}/")
 )
-data_quality_checks_silver_df.createOrReplaceTempView("data_quality_checks_silver")
 
-# ---------------------------
+# ------------------------------------------------------------
+# Create temp views
+# ------------------------------------------------------------
+tsdk_df.createOrReplaceTempView("trial_site_daily_kpis_silver")
+tspf_df.createOrReplaceTempView("trial_site_performance_flags_silver")
+tsmt_df.createOrReplaceTempView("trial_site_metric_trends_silver")
+dfqs_df.createOrReplaceTempView("data_freshness_quality_silver")
+
+# ------------------------------------------------------------
 # Target: gold_clinical_trial_kpis
-# ---------------------------
-
+# Latest row per (trial_id, country, site_id) by most recent kpi_date
+# ------------------------------------------------------------
 gold_clinical_trial_kpis_df = spark.sql(
     """
+    WITH ranked AS (
+      SELECT
+        tsdk.trial_id AS trial_id,
+        tsdk.country AS country,
+        tsdk.site_id AS site_id,
+        CAST(tsdk.total_enrolled_patients AS INT) AS total_enrolled_patients,
+        CAST(tsdk.active_patients AS INT) AS active_patients,
+        CAST(tsdk.visit_completion_percentage AS FLOAT) AS visit_completion_percentage,
+        ROW_NUMBER() OVER (
+          PARTITION BY tsdk.trial_id, tsdk.country, tsdk.site_id
+          ORDER BY DATE(tsdk.kpi_date) DESC
+        ) AS rn
+      FROM trial_site_daily_kpis_silver tsdk
+    )
     SELECT
-        CAST(ctss.trial_id AS STRING) AS trial_id,
-        CAST(ctss.country AS STRING) AS country,
-        CAST(ctss.site_id AS STRING) AS site_id,
-        CAST(ctss.total_enrolled_patients AS INT) AS total_enrolled_patients,
-        CAST(ctss.visit_completion_rate AS DOUBLE) AS visit_completion_percentage,
-        DATE(ctss.metric_date) AS metric_date
-    FROM clinical_trial_sites_silver ctss
+      trial_id,
+      country,
+      site_id,
+      total_enrolled_patients,
+      active_patients,
+      visit_completion_percentage
+    FROM ranked
+    WHERE rn = 1
     """
 )
 
@@ -74,56 +92,88 @@ gold_clinical_trial_kpis_df = spark.sql(
     .save(f"{TARGET_PATH}/gold_clinical_trial_kpis.csv")
 )
 
-# ---------------------------
-# Target: gold_trial_performance_analysis
-# ---------------------------
-
-gold_trial_performance_analysis_df = spark.sql(
+# ------------------------------------------------------------
+# Target: gold_clinical_trial_performance
+# Latest row per (trial_id, country, site_id) by most recent as_of_date
+# ------------------------------------------------------------
+gold_clinical_trial_performance_df = spark.sql(
     """
+    WITH ranked AS (
+      SELECT
+        tspf.trial_id AS trial_id,
+        tspf.country AS country,
+        tspf.site_id AS site_id,
+        CAST(tspf.enrollment_delay_flag AS BOOLEAN) AS enrollment_delay_flag,
+        CAST(tspf.underperforming_site_flag AS BOOLEAN) AS underperforming_site_flag,
+        ROW_NUMBER() OVER (
+          PARTITION BY tspf.trial_id, tspf.country, tspf.site_id
+          ORDER BY DATE(tspf.as_of_date) DESC
+        ) AS rn
+      FROM trial_site_performance_flags_silver tspf
+    )
     SELECT
-        CAST(pts.trial_id AS STRING) AS trial_id,
-        CAST(teas.site_id AS STRING) AS underperforming_site_id,
-        CAST(teas.enrollment_delay_flag AS INT) AS enrollment_delay_flag,
-        CAST(pts.trend_data AS STRING) AS trend_data,
-        DATE(pts.analysis_date) AS analysis_date
-    FROM performance_trends_silver pts
-    INNER JOIN trial_enrollment_analysis_silver teas
-        ON pts.trial_id = teas.trial_id
-       AND pts.site_id = teas.site_id
-       AND pts.analysis_date = teas.metric_date
+      trial_id,
+      country,
+      site_id,
+      enrollment_delay_flag,
+      underperforming_site_flag
+    FROM ranked
+    WHERE rn = 1
     """
 )
 
 (
-    gold_trial_performance_analysis_df.coalesce(1)
+    gold_clinical_trial_performance_df.coalesce(1)
     .write.mode("overwrite")
     .format("csv")
     .option("header", "true")
-    .save(f"{TARGET_PATH}/gold_trial_performance_analysis.csv")
+    .save(f"{TARGET_PATH}/gold_clinical_trial_performance.csv")
 )
 
-# ---------------------------
-# Target: gold_data_quality_metrics
-# ---------------------------
-
-gold_data_quality_metrics_df = spark.sql(
+# ------------------------------------------------------------
+# Target: gold_clinical_trial_trends
+# Map metric_date -> date
+# ------------------------------------------------------------
+gold_clinical_trial_trends_df = spark.sql(
     """
     SELECT
-        CAST(dqcs.data_quality_score AS DOUBLE) AS data_quality_score,
-        CAST(dqcs.reporting_latency AS DOUBLE) AS reporting_latency,
-        CAST(dqcs.data_freshness_percentage AS DOUBLE) AS data_freshness_percentage,
-        CAST(dqcs.processing_time AS DOUBLE) AS processing_time,
-        DATE(dqcs.evaluation_date) AS evaluation_date
-    FROM data_quality_checks_silver dqcs
+      tsmt.trial_id AS trial_id,
+      tsmt.country AS country,
+      tsmt.site_id AS site_id,
+      tsmt.metric_name AS metric_name,
+      tsmt.metric_value AS metric_value,
+      DATE(tsmt.metric_date) AS date
+    FROM trial_site_metric_trends_silver tsmt
     """
 )
 
 (
-    gold_data_quality_metrics_df.coalesce(1)
+    gold_clinical_trial_trends_df.coalesce(1)
     .write.mode("overwrite")
     .format("csv")
     .option("header", "true")
-    .save(f"{TARGET_PATH}/gold_data_quality_metrics.csv")
+    .save(f"{TARGET_PATH}/gold_clinical_trial_trends.csv")
+)
+
+# ------------------------------------------------------------
+# Target: gold_data_freshness_and_quality
+# ------------------------------------------------------------
+gold_data_freshness_and_quality_df = spark.sql(
+    """
+    SELECT
+      CAST(dfqs.data_update_time AS TIMESTAMP) AS data_update_time,
+      CAST(dfqs.data_quality_score AS FLOAT) AS data_quality_score,
+      CAST(dfqs.reporting_latency AS INT) AS reporting_latency
+    FROM data_freshness_quality_silver dfqs
+    """
+)
+
+(
+    gold_data_freshness_and_quality_df.coalesce(1)
+    .write.mode("overwrite")
+    .format("csv")
+    .option("header", "true")
+    .save(f"{TARGET_PATH}/gold_data_freshness_and_quality.csv")
 )
 
 job.commit()
