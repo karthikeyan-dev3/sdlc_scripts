1fb3ba4f36e6ae4991d81d3cf0d4d5740c8135ec
karthikeyan-dev3 <karthi.online03@gmail.com>
2026-05-20 18:48:49 +0530

    Add/Update /tmp/sdlc_downloads/gold_script.py

--- gold/gold_script.py
+++ gold/gold_script.py
@@ -1,8 +1,9 @@
 import sys
-from awsglue.context import GlueContext
-from awsglue.job import Job
+from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
 from pyspark.sql import SparkSession
 
 args = getResolvedOptions(sys.argv, ["JOB_NAME"])
@@ -17,78 +18,97 @@ SOURCE_PATH = "s3://sdlc-agent-bucket/engineering-agent/silver/"
 TARGET_PATH = "s3://sdlc-agent-bucket/engineering-agent/gold/"
 FILE_FORMAT = "csv"
 
-# ------------------------------------------------------------------------------
-# Read Source Tables (S3) + Temp Views
-# ------------------------------------------------------------------------------
-
-ctscs_df = (
+# -------------------------------------------------------------------
+# 1) Read source tables from S3
+# -------------------------------------------------------------------
+tscdm_df = (
     spark.read.format(FILE_FORMAT)
     .option("header", "true")
-    .load(f"{SOURCE_PATH}/clinical_trial_site_current_silver.{FILE_FORMAT}/")
+    .load(f"{SOURCE_PATH}/trial_site_country_daily_metrics_silver.{FILE_FORMAT}/")
 )
-ctscs_df.createOrReplaceTempView("clinical_trial_site_current_silver")
 
-ctsds_df = (
+tdils_df = (
     spark.read.format(FILE_FORMAT)
     .option("header", "true")
-    .load(f"{SOURCE_PATH}/clinical_trial_site_day_silver.{FILE_FORMAT}/")
-)
-ctsds_df.createOrReplaceTempView("clinical_trial_site_day_silver")
-
-# ------------------------------------------------------------------------------
-# Target: gold.clinical_trial_metrics
-# ------------------------------------------------------------------------------
-
-clinical_trial_metrics_df = spark.sql(
-    """
-    SELECT
-        CAST(ctscs.trial_id AS STRING)                      AS trial_id,
-        CAST(ctscs.country AS STRING)                       AS country,
-        CAST(ctscs.site_id AS STRING)                       AS site_id,
-        CAST(ctscs.total_enrolled_patients AS BIGINT)       AS total_enrolled_patients,
-        CAST(ctscs.active_patients AS BIGINT)               AS active_patients,
-        CAST(ctscs.patient_dropout_count AS BIGINT)         AS patient_dropout_count,
-        CAST(ctscs.visit_completion_percentage AS DOUBLE)   AS visit_completion_percentage,
-        CAST(ctscs.enrollment_trend AS DOUBLE)              AS enrollment_trend,
-        CAST(ctscs.underperforming_sites_flag AS BOOLEAN)   AS underperforming_sites_flag,
-        CAST(ctscs.enrollment_delay_flag AS BOOLEAN)        AS enrollment_delay_flag,
-        CAST(ctscs.patient_retention_rate AS DOUBLE)        AS patient_retention_rate,
-        CAST(ctscs.visit_adherence_rate AS DOUBLE)          AS visit_adherence_rate,
-        CAST(ctscs.data_refresh_time AS TIMESTAMP)          AS data_refresh_time
-    FROM clinical_trial_site_current_silver ctscs
-    """
+    .load(f"{SOURCE_PATH}/trial_data_ingestion_latency_silver.{FILE_FORMAT}/")
 )
 
+# -------------------------------------------------------------------
+# 2) Create temp views
+# -------------------------------------------------------------------
+tscdm_df.createOrReplaceTempView("trial_site_country_daily_metrics_silver")
+tdils_df.createOrReplaceTempView("trial_data_ingestion_latency_silver")
+
+# -------------------------------------------------------------------
+# 3) Transformations using Spark SQL
+# -------------------------------------------------------------------
+
+# Target: gold.gold_clinical_trial_summary
+gold_clinical_trial_summary_df = spark.sql("""
+SELECT
+  CAST(tscdm.trial_id AS STRING) AS trial_id,
+  CAST(tscdm.country AS STRING) AS country,
+  CAST(tscdm.site_id AS STRING) AS site_id,
+  SUM(CAST(tscdm.total_enrolled_patients AS BIGINT)) AS total_enrolled_patients,
+  SUM(CAST(tscdm.active_patients AS BIGINT)) AS active_patients,
+  SUM(CAST(tscdm.patient_dropout_count AS BIGINT)) AS patient_dropout_count,
+  AVG(CAST(tscdm.visit_completion_percentage AS DOUBLE)) AS visit_completion_percentage,
+  SUM(CAST(tscdm.enrollment_trend AS BIGINT)) AS enrollment_trend
+FROM trial_site_country_daily_metrics_silver tscdm
+GROUP BY
+  CAST(tscdm.trial_id AS STRING),
+  CAST(tscdm.country AS STRING),
+  CAST(tscdm.site_id AS STRING)
+""")
+
+# Target: gold.gold_clinical_trial_real_time_data
+gold_clinical_trial_real_time_data_df = spark.sql("""
+SELECT
+  CAST(tdils.trial_id AS STRING) AS trial_id,
+  MAX(CAST(tdils.data_ingestion_timestamp AS TIMESTAMP)) AS data_ingestion_timestamp,
+  MAX(tdils.metric_update_latency) AS metric_update_latency
+FROM trial_data_ingestion_latency_silver tdils
+GROUP BY
+  CAST(tdils.trial_id AS STRING)
+""")
+
+# Target: gold.gold_clinical_trial_historical_trends
+gold_clinical_trial_historical_trends_df = spark.sql("""
+SELECT
+  CAST(tscdm.trial_id AS STRING) AS trial_id,
+  CAST(tscdm.country AS STRING) AS country,
+  CAST(tscdm.site_id AS STRING) AS site_id,
+  DATE(CAST(tscdm.date AS DATE)) AS date,
+  CAST(tscdm.enrollment_trend AS BIGINT) AS enrollment_trend,
+  CAST(tscdm.retention_trend AS BIGINT) AS retention_trend
+FROM trial_site_country_daily_metrics_silver tscdm
+""")
+
+# -------------------------------------------------------------------
+# 4) Save outputs (single CSV file directly under TARGET_PATH)
+# -------------------------------------------------------------------
 (
-    clinical_trial_metrics_df.coalesce(1)
+    gold_clinical_trial_summary_df.coalesce(1)
     .write.mode("overwrite")
     .format("csv")
     .option("header", "true")
-    .save(f"{TARGET_PATH}/clinical_trial_metrics.csv")
-)
-
-# ------------------------------------------------------------------------------
-# Target: gold.clinical_trial_performance_trends
-# ------------------------------------------------------------------------------
-
-clinical_trial_performance_trends_df = spark.sql(
-    """
-    SELECT
-        CAST(ctsds.trial_id AS STRING)                       AS trial_id,
-        CAST(ctsds.trend_date AS DATE)                       AS trend_date,
-        CAST(ctsds.enrollment_trend_value AS DOUBLE)         AS enrollment_trend,
-        CAST(ctsds.dropout_trend_value AS DOUBLE)            AS dropout_trend,
-        CAST(ctsds.visit_completion_trend_value AS DOUBLE)   AS visit_completion_trend
-    FROM clinical_trial_site_day_silver ctsds
-    """
+    .save(f"{TARGET_PATH}/gold_clinical_trial_summary.csv")
 )
 
 (
-    clinical_trial_performance_trends_df.coalesce(1)
+    gold_clinical_trial_real_time_data_df.coalesce(1)
     .write.mode("overwrite")
     .format("csv")
     .option("header", "true")
-    .save(f"{TARGET_PATH}/clinical_trial_performance_trends.csv")
+    .save(f"{TARGET_PATH}/gold_clinical_trial_real_time_data.csv")
+)
+
+(
+    gold_clinical_trial_historical_trends_df.coalesce(1)
+    .write.mode("overwrite")
+    .format("csv")
+    .option("header", "true")
+    .save(f"{TARGET_PATH}/gold_clinical_trial_historical_trends.csv")
 )
 
 job.commit()
