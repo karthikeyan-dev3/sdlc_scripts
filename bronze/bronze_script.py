9728771726a47ba70fa37e9459dd27c47d1d03d8
karthikeyan-dev3 <karthi.online03@gmail.com>
2026-05-25 15:15:06 +0530

    Add/Update tmp/corrected_scripts/a2e14db4-03b2-437a-8ecd-210a41018dbf/bronze_script.py

--- bronze/bronze_script.py
+++ bronze/bronze_script.py
@@ -1,7 +1,6 @@
-```python
 from awsglue.context import GlueContext
 from pyspark.context import SparkContext
-from awsglue.job import J
+from awsglue.job import Job
 
 sc = SparkContext()
 glueContext = GlueContext(sc)
@@ -47,5 +46,3 @@ for table in metadata['tables']:
     writer.save(target_path + target_table + "." + write_format)
 
 job.commit()
-
-```
