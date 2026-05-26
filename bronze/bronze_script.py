25419809f58e7f41ca5b3d579d58b77d32c7d7a6
karthikeyan-dev3 <karthi.online03@gmail.com>
2026-05-26 17:18:04 +0530

    Add/Update /tmp/sdlc_downloads/bronze_script.py

--- bronze/bronze_script.py
+++ bronze/bronze_script.py
@@ -1,8 +1,9 @@
+```python
 from awsglue.context import GlueContext
 from pyspark.context import SparkContext
 from awsglue.job import Job
 
-sc = SparkContext()
+
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 
@@ -13,24 +14,24 @@ metadata = {
     'tables': [
         {
             'target_schema': 'bronze',
-            'target_table': 'products_bronze',
-            'target_alias': 'pb',
+            'target_table': 'master_product_bronze',
+            'target_alias': 'mpb',
             'mapping_details': 'products_raw pr',
-            'description': 'Bronze-level ingestion of product master data from products_raw. Columns mapped 1:1: product_id, product_name, category, brand, price, is_active.'
+            'description': 'Bronze master product table sourced directly from products_raw. Columns: product_id, product_name, category, brand, price, is_active.'
         },
         {
             'target_schema': 'bronze',
-            'target_table': 'stores_bronze',
-            'target_alias': 'sb',
+            'target_table': 'master_store_bronze',
+            'target_alias': 'msb',
             'mapping_details': 'stores_raw sr',
-            'description': 'Bronze-level ingestion of store master data from stores_raw. Columns mapped 1:1: store_id, store_name, city, state, store_type, open_date.'
+            'description': 'Bronze master store table sourced directly from stores_raw. Columns: store_id, store_name, city, state, store_type, open_date.'
         },
         {
             'target_schema': 'bronze',
-            'target_table': 'sales_transactions_bronze',
-            'target_alias': 'stb',
+            'target_table': 'sales_performance_bronze',
+            'target_alias': 'spb',
             'mapping_details': 'sales_transactions_raw str',
-            'description': 'Bronze-level ingestion of sales transaction records from sales_transactions_raw. Columns mapped 1:1: transaction_id, store_id, product_id, quantity, sale_amount, transaction_time.'
+            'description': 'Bronze sales performance transactional table sourced directly from sales_transactions_raw. Columns: transaction_id, store_id, product_id, quantity, sale_amount, transaction_time.'
         }
     ],
     'columns': [
@@ -41,8 +42,8 @@ metadata = {
             'target_column': 'product_id',
             'target_type': 'varchar(10)',
             'target_nullable': 'not specified',
-            'transformation': 'pb.product_id = pr.product_id',
-            'target_table': 'pb'
+            'transformation': 'pr.product_id = mpb.product_id',
+            'target_table': 'pr'
         },
         {
             'source_column': "['pr.product_name']",
@@ -51,8 +52,8 @@ metadata = {
             'target_column': 'product_name',
             'target_type': 'varchar(255)',
             'target_nullable': 'not specified',
-            'transformation': 'pb.product_name = pr.product_name',
-            'target_table': 'pb'
+            'transformation': 'pr.product_name = mpb.product_name',
+            'target_table': 'pr'
         },
         {
             'source_column': "['pr.category']",
@@ -61,8 +62,8 @@ metadata = {
             'target_column': 'category',
             'target_type': 'varchar(100)',
             'target_nullable': 'not specified',
-            'transformation': 'pb.category = pr.category',
-            'target_table': 'pb'
+            'transformation': 'pr.category = mpb.category',
+            'target_table': 'pr'
         },
         {
             'source_column': "['pr.brand']",
@@ -71,8 +72,8 @@ metadata = {
             'target_column': 'brand',
             'target_type': 'varchar(100)',
             'target_nullable': 'not specified',
-            'transformation': 'pb.brand = pr.brand',
-            'target_table': 'pb'
+            'transformation': 'pr.brand = mpb.brand',
+            'target_table': 'pr'
         },
         {
             'source_column': "['pr.price']",
@@ -81,8 +82,8 @@ metadata = {
             'target_column': 'price',
             'target_type': 'float',
             'target_nullable': 'not specified',
-            'transformation': 'pb.price = pr.price',
-            'target_table': 'pb'
+            'transformation': 'pr.price = mpb.price',
+            'target_table': 'pr'
         },
         {
             'source_column': "['pr.is_active']",
@@ -91,8 +92,8 @@ metadata = {
             'target_column': 'is_active',
             'target_type': 'boolean',
             'target_nullable': 'not specified',
-            'transformation': 'pb.is_active = pr.is_active',
-            'target_table': 'pb'
+            'transformation': 'pr.is_active = mpb.is_active',
+            'target_table': 'pr'
         },
         {
             'source_column': "['sr.store_id']",
@@ -101,8 +102,8 @@ metadata = {
             'target_column': 'store_id',
             'target_type': 'varchar(10)',
             'target_nullable': 'not specified',
-            'transformation': 'sb.store_id = sr.store_id',
-            'target_table': 'sb'
+            'transformation': 'sr.store_id = msb.store_id',
+            'target_table': 'sr'
         },
         {
             'source_column': "['sr.store_name']",
@@ -111,8 +112,8 @@ metadata = {
             'target_column': 'store_name',
             'target_type': 'varchar(255)',
             'target_nullable': 'not specified',
-            'transformation': 'sb.store_name = sr.store_name',
-            'target_table': 'sb'
+            'transformation': 'sr.store_name = msb.store_name',
+            'target_table': 'sr'
         },
         {
             'source_column': "['sr.city']",
@@ -121,8 +122,8 @@ metadata = {
             'target_column': 'city',
             'target_type': 'varchar(100)',
             'target_nullable': 'not specified',
-            'transformation': 'sb.city = sr.city',
-            'target_table': 'sb'
+            'transformation': 'sr.city = msb.city',
+            'target_table': 'sr'
         },
         {
             'source_column': "['sr.state']",
@@ -131,8 +132,8 @@ metadata = {
             'target_column': 'state',
             'target_type': 'varchar(100)',
             'target_nullable': 'not specified',
-            'transformation': 'sb.state = sr.state',
-            'target_table': 'sb'
+            'transformation': 'sr.state = msb.state',
+            'target_table': 'sr'
         },
         {
             'source_column': "['sr.store_type']",
@@ -141,8 +142,8 @@ metadata = {
             'target_column': 'store_type',
             'target_type': 'varchar(50)',
             'target_nullable': 'not specified',
-            'transformation': 'sb.store_type = sr.store_type',
-            'target_table': 'sb'
+            'transformation': 'sr.store_type = msb.store_type',
+            'target_table': 'sr'
         },
         {
             'source_column': "['sr.open_date']",
@@ -151,8 +152,8 @@ metadata = {
             'target_column': 'open_date',
             'target_type': 'date',
             'target_nullable': 'not specified',
-            'transformation': 'sb.open_date = sr.open_date',
-            'target_table': 'sb'
+            'transformation': 'sr.open_date = msb.open_date',
+            'target_table': 'sr'
         },
         {
             'source_column': "['str.transaction_id']",
@@ -161,8 +162,8 @@ metadata = {
             'target_column': 'transaction_id',
             'target_type': 'varchar(10)',
             'target_nullable': 'not specified',
-            'transformation': 'stb.transaction_id = str.transaction_id',
-            'target_table': 'stb'
+            'transformation': 'str.transaction_id = spb.transaction_id',
+            'target_table': 'str'
         },
         {
             'source_column': "['str.store_id']",
@@ -171,8 +172,8 @@ metadata = {
             'target_column': 'store_id',
             'target_type': 'varchar(10)',
             'target_nullable': 'not specified',
-            'transformation': 'stb.store_id = str.store_id',
-            'target_table': 'stb'
+            'transformation': 'str.store_id = spb.store_id',
+            'target_table': 'str'
         },
         {
             'source_column': "['str.product_id']",
@@ -181,8 +182,8 @@ metadata = {
             'target_column': 'product_id',
             'target_type': 'varchar(10)',
             'target_nullable': 'not specified',
-            'transformation': 'stb.product_id = str.product_id',
-            'target_table': 'stb'
+            'transformation': 'str.product_id = spb.product_id',
+            'target_table': 'str'
         },
         {
             'source_column': "['str.quantity']",
@@ -191,8 +192,8 @@ metadata = {
             'target_column': 'quantity',
             'target_type': 'int',
             'target_nullable': 'not specified',
-            'transformation': 'stb.quantity = str.quantity',
-            'target_table': 'stb'
+            'transformation': 'str.quantity = spb.quantity',
+            'target_table': 'str'
         },
         {
             'source_column': "['str.sale_amount']",
@@ -201,8 +202,8 @@ metadata = {
             'target_column': 'sale_amount',
             'target_type': 'double',
             'target_nullable': 'not specified',
-            'transformation': 'stb.sale_amount = str.sale_amount',
-            'target_table': 'stb'
+            'transformation': 'str.sale_amount = spb.sale_amount',
+            'target_table': 'str'
         },
         {
             'source_column': "['str.transaction_time']",
@@ -211,8 +212,8 @@ metadata = {
             'target_column': 'transaction_time',
             'target_type': 'timestamp',
             'target_nullable': 'not specified',
-            'transformation': 'stb.transaction_time = str.transaction_time',
-            'target_table': 'stb'
+            'transformation': 'str.transaction_time = spb.transaction_time',
+            'target_table': 'str'
         }
     ],
     'runtime_config': {
@@ -231,32 +232,38 @@ write_format = metadata['runtime_config']['write_format']
 write_mode = metadata['runtime_config']['write_mode']
 
 for table in metadata['tables']:
-    mapping_details = table['mapping_details'].split()
-    source_table = mapping_details[0]
-    source_alias = mapping_details[1]
+    mapping_details = table['mapping_details']
+    source_table = mapping_details.split()[0]
+    source_alias = mapping_details.split()[1]
     target_table = table['target_table']
     target_alias = table['target_alias']
 
     reader = spark.read.format(read_format)
     if read_format == 'csv':
-        reader = reader.option('header', 'true').option('inferSchema', 'true')
+        reader = reader.option("header", "true").option("inferSchema", "true")
 
-    df = reader.load(base_path + source_table + '.' + read_format)
+    df = reader.load(base_path + f"{source_table}.{read_format}")
     df = df.alias(source_alias)
 
     transformations = []
     for col_meta in metadata['columns']:
-        if col_meta['target_table'] == target_alias:
-            rhs = col_meta['transformation'].split('=', 1)[1].strip()
-            target_col = col_meta['target_column']
-            transformations.append(f"{rhs} as {target_col}")
+        if col_meta.get('target_table') == source_alias:
+            transformation = col_meta.get('transformation', '')
+            if '=' in transformation:
+                rhs = transformation.split('=', 1)[0].strip()
+            else:
+                rhs = transformation.strip()
+            target_column = col_meta.get('target_column')
+            transformations.append(f"{rhs} as {target_column}")
 
     df = df.selectExpr(*transformations)
 
     writer = df.write.mode(write_mode).format(write_format)
     if write_format == 'csv':
-        writer = writer.option('header', 'true')
+        writer = writer.option("header", "true")
 
-    writer.save(target_path + target_table + '.' + write_format)
+    writer.save(target_path + f"{target_table}.{write_format}")
 
 job.commit()
+
+```
