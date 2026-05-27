54934f30a5c410b7d285e0089b2107c568876aaa
karthikeyan-dev3 <karthi.online03@gmail.com>
2026-05-27 20:51:10 +0530

    Add/Update tmp/corrected_scripts/b7967e11-e142-4383-b9d1-4833ba685a94/bronze_script.py

--- bronze/bronze_script.py
+++ bronze/bronze_script.py
@@ -2,7 +2,7 @@ from awsglue.context import GlueContext
 from pyspark.context import SparkContext
 from awsglue.job import Job
 
-
+sc = SparkContext.getOrCreate()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 
