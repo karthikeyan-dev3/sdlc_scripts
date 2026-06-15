5e78faf2e97c5a6e4186b16f80674abe8842f5da
karthikeyan-dev3 <karthi.online03@gmail.com>
2026-06-15 13:45:58 +0530

    Add/Update /tmp/sdlc_downloads/bronze_script.py

--- bronze/bronze_script.py
+++ bronze/bronze_script.py
@@ -3,10 +3,11 @@ from awsglue.context import GlueContext
 from awsglue.job import Job
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
+from pyspark.sql import SparkSession
 
 args = getResolvedOptions(sys.argv, ["JOB_NAME"])
 
-sc = SparkContext()
+sc = SparkContext.getOrCreate()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
@@ -16,55 +17,45 @@ SOURCE_PATH = "s3://sdlc-agent-bucket/engineering-agent/src/"
 TARGET_PATH = "s3://sdlc-agent-bucket/engineering-agent/bronze/"
 FILE_FORMAT = "csv"
 
-# -----------------------------
-# products_bronze
-# -----------------------------
-products_raw_df = (
-    spark.read.format(FILE_FORMAT)
-    .option("header", "true")
-    .load(f"{SOURCE_PATH}/products_raw.{FILE_FORMAT}/")
-)
-products_raw_df.createOrReplaceTempView("products_raw")
-
-products_bronze_df = spark.sql(
-    """
-    SELECT
-        CAST(pr.product_id AS STRING)   AS product_id,
-        CAST(pr.product_name AS STRING) AS product_name,
-        CAST(pr.category AS STRING)     AS category,
-        CAST(pr.brand AS STRING)        AS brand,
-        CAST(pr.price AS DOUBLE)        AS price,
-        CAST(pr.is_active AS BOOLEAN)   AS is_active
-    FROM products_raw pr
-    """
-)
-
-(
-    products_bronze_df.coalesce(1)
-    .write.mode("overwrite")
-    .format("csv")
-    .option("header", "true")
-    .save(f"{TARGET_PATH}/products_bronze.csv")
-)
-
-# -----------------------------------
-# sales_transactions_bronze
-# -----------------------------------
+# -----------------------------------------------------------------------------------
+# Read Source Tables
+# -----------------------------------------------------------------------------------
 sales_transactions_raw_df = (
     spark.read.format(FILE_FORMAT)
     .option("header", "true")
     .load(f"{SOURCE_PATH}/sales_transactions_raw.{FILE_FORMAT}/")
 )
-sales_transactions_raw_df.createOrReplaceTempView("sales_transactions_raw")
 
+products_raw_df = (
+    spark.read.format(FILE_FORMAT)
+    .option("header", "true")
+    .load(f"{SOURCE_PATH}/products_raw.{FILE_FORMAT}/")
+)
+
+stores_raw_df = (
+    spark.read.format(FILE_FORMAT)
+    .option("header", "true")
+    .load(f"{SOURCE_PATH}/stores_raw.{FILE_FORMAT}/")
+)
+
+# -----------------------------------------------------------------------------------
+# Create Temp Views
+# -----------------------------------------------------------------------------------
+sales_transactions_raw_df.createOrReplaceTempView("sales_transactions_raw")
+products_raw_df.createOrReplaceTempView("products_raw")
+stores_raw_df.createOrReplaceTempView("stores_raw")
+
+# -----------------------------------------------------------------------------------
+# Target: bronze.sales_transactions_bronze
+# -----------------------------------------------------------------------------------
 sales_transactions_bronze_df = spark.sql(
     """
     SELECT
-        CAST(str.transaction_id AS STRING)     AS transaction_id,
-        CAST(str.store_id AS STRING)           AS store_id,
-        CAST(str.product_id AS STRING)         AS product_id,
-        CAST(str.quantity AS INT)              AS quantity,
-        CAST(str.sale_amount AS DOUBLE)        AS sale_amount,
+        CAST(str.transaction_id AS STRING)   AS transaction_id,
+        CAST(str.store_id AS STRING)         AS store_id,
+        CAST(str.product_id AS STRING)       AS product_id,
+        CAST(str.quantity AS INT)            AS quantity,
+        CAST(str.sale_amount AS DOUBLE)      AS sale_amount,
         CAST(str.transaction_time AS TIMESTAMP) AS transaction_time
     FROM sales_transactions_raw str
     """
@@ -78,35 +69,52 @@ sales_transactions_bronze_df = spark.sql(
     .save(f"{TARGET_PATH}/sales_transactions_bronze.csv")
 )
 
-# -----------------------------
-# stores_bronze
-# -----------------------------
-stores_raw_df = (
-    spark.read.format(FILE_FORMAT)
-    .option("header", "true")
-    .load(f"{SOURCE_PATH}/stores_raw.{FILE_FORMAT}/")
-)
-stores_raw_df.createOrReplaceTempView("stores_raw")
-
-stores_bronze_df = spark.sql(
+# -----------------------------------------------------------------------------------
+# Target: bronze.product_bronze
+# -----------------------------------------------------------------------------------
+product_bronze_df = spark.sql(
     """
     SELECT
-        CAST(sr.store_id AS STRING)    AS store_id,
-        CAST(sr.store_name AS STRING)  AS store_name,
-        CAST(sr.city AS STRING)        AS city,
-        CAST(sr.state AS STRING)       AS state,
-        CAST(sr.store_type AS STRING)  AS store_type,
-        DATE(sr.open_date)            AS open_date
+        CAST(pr.product_id AS STRING)     AS product_id,
+        CAST(pr.product_name AS STRING)   AS product_name,
+        CAST(pr.category AS STRING)       AS category,
+        CAST(pr.brand AS STRING)          AS brand,
+        CAST(pr.price AS FLOAT)           AS price,
+        CAST(pr.is_active AS BOOLEAN)     AS is_active
+    FROM products_raw pr
+    """
+)
+
+(
+    product_bronze_df.coalesce(1)
+    .write.mode("overwrite")
+    .format("csv")
+    .option("header", "true")
+    .save(f"{TARGET_PATH}/product_bronze.csv")
+)
+
+# -----------------------------------------------------------------------------------
+# Target: bronze.store_bronze
+# -----------------------------------------------------------------------------------
+store_bronze_df = spark.sql(
+    """
+    SELECT
+        CAST(sr.store_id AS STRING)     AS store_id,
+        CAST(sr.store_name AS STRING)   AS store_name,
+        CAST(sr.city AS STRING)         AS city,
+        CAST(sr.state AS STRING)        AS state,
+        CAST(sr.store_type AS STRING)   AS store_type,
+        CAST(sr.open_date AS DATE)      AS open_date
     FROM stores_raw sr
     """
 )
 
 (
-    stores_bronze_df.coalesce(1)
+    store_bronze_df.coalesce(1)
     .write.mode("overwrite")
     .format("csv")
     .option("header", "true")
-    .save(f"{TARGET_PATH}/stores_bronze.csv")
+    .save(f"{TARGET_PATH}/store_bronze.csv")
 )
 
 job.commit()
